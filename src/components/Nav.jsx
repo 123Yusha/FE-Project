ac847d11f89f198c1b01f62afce7bc53bb7efe01
123Yusha <yr130488@googlemail.com>
2024-06-17 17:02:31 +0100

    created dropdown in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export function Nav() {
-
+return (
+    <nav>
+        <select
+        name="topics"
+        id="topics"
+        onChange={handleChange}>
+          <option value="articles">All Items</option>
+          <option value="coding">Clothing Articles</option>
+          <option value="cooking">Electronics Articles</option>
+          <option value="football">Household Articles</option>
+        </select>
+    </nav>
+)
 }
